ae476169de6c5c20eaab5f165f95c3e436e33c1b
Matthias Nagel <matthinagel@gmail.com>
2019-09-13 10:27:46 +0200

    Address updated

--- src/MyContent.hs
+++ src/MyContent.hs
@@ -37,10 +37,9 @@ researchBlock = do
             p "My field of study is geometric topology. I am interested in concordance of links, and surfaces of minimal genus in 3- and 4-manifolds."
             p "One of my favourite tools for studying these questions is the signature and twisted Reidemeister torsion."
 
-oxaddress = pre "nagel (at) maths.ox.ac.uk\n\n\
-        \Mathematical Institute\n\
-        \University of Oxford\n\
-        \Oxford, United Kingdom"
+oxaddress = pre "matthias.nagel (at) math.ethz.ch\n\n\
+        \Department of Mathematics\n\
+        \ETH Zurich, Switzerland"
 
 aboutmeBlock :: Html
 aboutmeBlock = rightPicture (oxaddress) "./images/myself2.jpg" "Photo of myself" "myphoto"
