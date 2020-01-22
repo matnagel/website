2bd66ce47f1098ea1c66b65ca4d5ce43f4feb006
Matthias Nagel <matthinagel@gmail.com>
2020-01-22 10:58:18 +0100

    Adds new publication

--- src/MyContent.hs
+++ src/MyContent.hs
@@ -36,6 +36,12 @@ researchBlock = headline "Interests"
             <> p "My field of study is geometric topology. I am interested in geometric objects up to dimension 4 and their embeddings. In particular, I study knots and their mathematical properties. Here is an example of an interesting (part of a) knot:"
             <> (flex ! style "justify-content:center; margin:2ex" $ image (storageETH "images/clasp.jpg") "Clasp" "clasp")
             <> p "I am especially curious about concordance, a special equivalence relation on knots. This relation is intimately tied to questions on the topology of 4-manifolds and their smooth structures."
+            <> projects
+
+projects = p 
+    $ "If you are interested in writing bachelor thesis or a semester project with me, here are some possible " 
+    <> link (storageETH "projects.pdf") "topics" 
+    <> "."
 
 ethaddress = pre "matthias.nagel (at) math.ethz.ch\n\n\
         \Department of Mathematics\n\
@@ -51,4 +57,4 @@ aboutmeContent = flex ! style "flex-direction:column"
         <> whoIam
 
 aboutmeBlock :: Html
-aboutmeBlock = rightPicture aboutmeContent (storageETH "images/myself2.jpg") "Photo of myself" "myphoto"
+aboutmeBlock = rightPicture aboutmeContent (storageETH "images/myself3.jpg") "Photo of myself" "myphoto"
