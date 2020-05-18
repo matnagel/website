a73810a9a18eb682dbb5ddaf9d39ed553caea1c0
Matthias Nagel <matthinagel@gmail.com>
2020-05-18 13:32:38 +0200

    Improves Parser

--- src/MarkLightParser.hs
+++ src/MarkLightParser.hs
@@ -5,6 +5,8 @@ module MarkLightParser
   )
 where
 
+import Control.Monad
+import qualified Data.Map as M
 import qualified Text.Parsec as P
 import Text.Parsec
   ( (<?>),
@@ -14,13 +16,13 @@ import Text.Parsec
     letter,
     many,
     many1,
+    manyTill,
     parse,
+    sourceColumn,
     space,
     spaces,
     string,
-    manyTill,
     try,
-    sourceColumn
   )
 import Text.Parsec.Char
 import Text.Parsec.Combinator
@@ -28,8 +30,6 @@ import Text.Parsec.Prim
 import Text.Parsec.String
 import Utils
 import Prelude hiding (div, head, id)
-
-import Control.Monad
 
 newtype URLPath = MkURLPath String deriving (Show)
 
@@ -39,11 +39,13 @@ newtype Name = MkName String deriving (Show)
 
 newtype Text = MkText String deriving (Show)
 
+newtype Options = MkOptions (M.Map String String) deriving (Show)
+
 data LightAtom
   = Word String
   | Newline
   | Picture URLPath Name
-  | Link URLPath Name
+  | Link URLPath Text
   deriving (Show)
 
 data LightBlock
@@ -57,33 +59,42 @@ instance Semigroup LightBlock where
   (<>) (Plain a) (Plain b) = Plain (a <> b)
   (<>) a@(Plain _) b = a <> Plain [b]
   (<>) a b@(Plain _) = Plain [a] <> b
-  (<>) a b = Plain [a,b]
+  (<>) a b = Plain [a, b]
 
 instance Monoid LightBlock where
-    mempty = Plain []
+  mempty = Plain []
 
-data PageInformation
-  = PageInformation
-      { getName :: Name,
-        getURL :: URLPath
-      } deriving (Show)
+data PageInformation = PageInformation
+  { getName :: Name,
+    getOutput :: LocalPath
+  }
+  deriving (Show)
 
 data Page = Page PageInformation LightBlock deriving (Show)
 
-startOfLine :: Monad m => ParsecT s u m ()
-startOfLine = do
-    pos <- getPosition
-    guard (sourceColumn pos == 1)
+maybeFail :: MonadFail m => Maybe a -> String -> m a
+maybeFail (Just x) _ = return x
+maybeFail Nothing str = fail str
 
-notAtStartOfLine :: Monad m => ParsecT s u m ()
-notAtStartOfLine = do
-    pos <- getPosition
-    guard (sourceColumn pos /= 1)
+getPageInformation :: MonadFail m => Options -> m PageInformation
+getPageInformation (MkOptions omap) = PageInformation
+    <$> maybeFail (MkName <$> M.lookup "name" omap) "Could not retrieve name"
+    <*> maybeFail (MkLocalPath <$> M.lookup "path" omap) "Could not retrieve path"
 
-redundantSpace = (notAtStartOfLine >> space) <|> char ' ' <|> tab
+ensureStartOfLine :: Monad m => ParsecT s u m ()
+ensureStartOfLine = do
+  pos <- getPosition
+  guard (sourceColumn pos == 1)
 
-lexemeAtom :: Parser a -> Parser a
-lexemeAtom p = p <* (many redundantSpace)
+preventStartOfLine :: Monad m => ParsecT s u m ()
+preventStartOfLine = do
+  pos <- getPosition
+  guard (sourceColumn pos /= 1)
+
+redundantSpace = (preventStartOfLine >> space) <|> char ' ' <|> tab
+
+lexemeRedundant :: Parser a -> Parser a
+lexemeRedundant p = p <* (many redundantSpace)
 
 lexemeBlock :: Parser a -> Parser a
 lexemeBlock p = p <* spaces
@@ -91,24 +102,62 @@ lexemeBlock p = p <* spaces
 wordLetter = alphaNum <|> char '.' <|> char ':'
 
 parseWord :: Parser LightAtom
-parseWord = lexemeAtom (Word <$> (many1 wordLetter))
+parseWord = lexemeRedundant (Word <$> (many1 wordLetter))
 
 emptyLine :: Parser LightAtom
 emptyLine = (newline) *> (return Newline)
 
+symbol a = lexemeRedundant $ char a
+
+stringToken str = lexemeRedundant $ string str
+
+urlLetters = lexemeRedundant $ many1 (alphaNum <|> char ':' <|> char '/')
+
+linkLetters = lexemeRedundant $ many1 (alphaNum <|> char ' ')
+
+keyletters = lexemeRedundant $ many1 alphaNum
+
+valueLetters = lexemeRedundant $ many1 (alphaNum <|> char ':' <|> char '/' <|> char ' ')
+
+braceCommand str p = lexemeRedundant $ between (symbol '{') (symbol '}') (stringToken str >> p)
+
+parseOptionsHelper :: Parser (String, String)
+parseOptionsHelper = lexemeRedundant $ do
+  key <- keyletters
+  symbol '='
+  value <- between (symbol '\"') (symbol '\"') valueLetters
+  return $ (key, value)
+
+parseOptions :: Parser Options
+parseOptions = lexemeRedundant $ do
+  opts <- many parseOptionsHelper
+  return $ MkOptions $ M.fromList opts
+
+parsePageInformation :: Parser PageInformation
+parsePageInformation = braceCommand "page" $ do
+  opts <- parseOptions
+  getPageInformation opts
+
+parseLink :: Parser LightAtom
+parseLink = braceCommand "link" $ do
+  url <- between (symbol '\"') (symbol '\"') urlLetters
+  name <- between (symbol '\"') (symbol '\"') linkLetters
+  return $ Link (MkURLPath url) (MkText name)
+
 parseHeader :: Parser LightBlock
-parseHeader = startOfLine *> do
-    lexemeAtom (char '=')
-    hdr <- many1 parseWord
-    return $ Header hdr
+parseHeader = ensureStartOfLine *> do
+  symbol '='
+  hdr <- many1 parseWord
+  return $ Header hdr
 
 parseParagraph :: Parser LightBlock
-parseParagraph = startOfLine *> do
-    text <- many1 parseWord
-    return $ Para text
+parseParagraph = ensureStartOfLine *> do
+  text <- many1 (parseWord <|> parseLink)
+  return $ Para text
 
-parsePage :: Parser LightBlock
+parsePage :: Parser Page
 parsePage = do
-    elms <- many $ lexemeBlock (parseHeader <|> parseParagraph)
-    eof
-    return $ mconcat elms
+  pageInformation <- parsePageInformation
+  elms <- many $ lexemeBlock (parseHeader <|> parseParagraph)
+  eof
+  return $ Page pageInformation $ mconcat elms
