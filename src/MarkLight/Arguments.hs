8323634d3e56451a545fe097f59fe4aaee838962
Matthias Nagel <matthinagel@gmail.com>
2020-05-24 19:22:15 +0200

    Improve argument handling

--- src/MarkLight/Arguments.hs
+++ src/MarkLight/Arguments.hs
@@ -35,12 +35,19 @@ import qualified Utils as U
 import Prelude hiding (div, head, id)
 
 newtype Arguments = MkArguments (M.Map String String)
+data AccumulateArguments = MkAcc [String] Arguments
 
 instance Semigroup Arguments where
   (<>) (MkArguments a) (MkArguments b) = MkArguments (b <> a)
 
 instance Monoid Arguments where
   mempty = MkArguments mempty
+
+instance Semigroup AccumulateArguments where
+  (<>) (MkAcc as arg) (MkAcc bs brg) = MkAcc (as <> bs) (arg <> brg)
+
+instance Monoid AccumulateArguments where
+  mempty = MkAcc [] mempty
 
 tokenize :: Parser a -> Parser a
 tokenize p = p <* spaces
@@ -62,6 +69,7 @@ instance Show Arguments where
 
 quote p = tokenize $ between (char '\"') (charToken '\"') p
 
+
 parseArgument :: Parser Arguments
 parseArgument = tokenize $ do
   key <- keyletters
@@ -69,10 +77,30 @@ parseArgument = tokenize $ do
   value <- quote valueLetters
   return $ MkArguments $ M.singleton key value
 
+-- parseArguments :: Parser Arguments
+-- parseArguments = tokenize $ do
+--   opts <- sepBy parseArgument $ optional (charToken ',')
+--   return $ mconcat opts
+
+parseUnassignedArgument :: Parser AccumulateArguments
+parseUnassignedArgument = tokenize $ do
+    value <- quote valueLetters
+    return $ MkAcc [value] mempty
+
+parseAccumulateArguments :: Parser AccumulateArguments
+parseAccumulateArguments = tokenize $ do
+    opts <- sepBy (parseUnassignedArgument
+                    <|> (MkAcc [] <$> parseArgument))$ optional (charToken ',')
+    return $ mconcat opts
+
+parseArgumentsWithKeys :: [String] -> Parser Arguments
+parseArgumentsWithKeys keys = tokenize $ do
+    (MkAcc ls args) <- parseAccumulateArguments
+    return $ args
+
 parseArguments :: Parser Arguments
-parseArguments = tokenize $ do
-  opts <- sepBy parseArgument $ optional (charToken ',')
-  return $ mconcat opts
+parseArguments = parseArgumentsWithKeys []
+
 
 getArgument :: String -> Arguments -> Maybe String
 getArgument str (MkArguments mp) = M.lookup str mp
