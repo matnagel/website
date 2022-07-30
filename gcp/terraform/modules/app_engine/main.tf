8d914bbef317fe9ad8768b3f1132c6d9e6b2cade
Matthias Nagel <matthinagel@gmail.com>
2022-07-30 18:51:34 +0200

    Adds noop

--- gcp/terraform/modules/app_engine/main.tf
+++ gcp/terraform/modules/app_engine/main.tf
@@ -44,6 +44,8 @@ resource "google_app_engine_standard_app_version" "website-app" {
   basic_scaling {
     max_instances = 2
   }
+
+  noop_on_destroy = true
 }
 
 resource "google_app_engine_application" "app-engine" {
