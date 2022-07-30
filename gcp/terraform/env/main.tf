465747a49f0133a8803e0180929a7947b08cb386
Matthias Nagel <matthinagel@gmail.com>
2022-07-30 14:30:38 +0200

    Fixes environment

--- gcp/terraform/env/main.tf
+++ gcp/terraform/env/main.tf
@@ -1,4 +1,10 @@
 provider "google" {
-  project = var.command-project
+  project = var.project
   zone = var.zone
+}
+
+module "website" {
+  source      = "../modules/website"
+  project  = var.project
+  region = var.region
 }
